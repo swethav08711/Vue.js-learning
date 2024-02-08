1070dee469658f459541d3cca70e7c9534ae1454
swethav08711 <shwe08711@gmail.com>
2024-02-08 19:17:21 +0530

    fetching data'

--- ninja-jobs/src/views/jobs/JobDetail.vue
+++ ninja-jobs/src/views/jobs/JobDetail.vue
@@ -1,15 +1,28 @@
 <template>
-  <h1>Job detail page {{ id }}</h1>
+  <div v-if="job">
+    <p>{{ job.description }}</p>
+    <p>{{ job.location }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Job details.....</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      // id: this.$route.params.id,
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
--- ninja-jobs/src/views/jobs/JobsView.vue
+++ ninja-jobs/src/views/jobs/JobsView.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>List of Jobs</h1>
-  <div class="job" v-for="job in jobs" :key="job.id">
-    <router-link :to="{ name: 'JobsDetail', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div class="job" v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobsDetail', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading List of Jobs.....</p>
   </div>
 </template>
 
@@ -11,36 +16,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        {
-          id: 1,
-          title: "Job Title 1",
-          description: "Job Description 1",
-          company: "Company 1",
-          location: "Location 1",
-          date: "2020-01-01",
-          status: "Pending",
-        },
-        {
-          id: 2,
-          title: "Job Title 2",
-          description: "Job Description 2",
-          company: "Company 2",
-          location: "Location 2",
-          date: "2020-01-02",
-          status: "Pending",
-        },
-        {
-          id: 3,
-          title: "Job Title 3",
-          description: "Job Description 3",
-          company: "Company 3",
-          location: "Location 3",
-          date: "2020-01-03",
-          status: "Pending",
-        },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
