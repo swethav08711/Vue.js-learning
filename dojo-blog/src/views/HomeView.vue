34ef38506270c034a3beb024adf76708a6deb4a8
swethav08711 <shwe08711@gmail.com>
2024-02-08 22:32:38 +0530

    learnt refs in vue

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>My name id {{ name }} my age is {{ age }}</p>
+    <button @click="handleClick">Click</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name" />
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    // const p = ref(null);
+
+    let name = ref("swetha");
+    let age = ref(24);
+    const handleClick = () => {
+      name.value = "purple";
+      age.value = 30;
+      console.log("hello");
+      // console.log(p.value);
+      // p.value.classList.add("test");
+      // p.value.textContent = "hello swetha";
+    };
+
+    return { name, age, handleClick };
+  },
+};
 </script>
+
+<style>
+.test {
+  color: red;
+}
+</style>
